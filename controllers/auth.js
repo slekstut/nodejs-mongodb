2e653ea77194f77276d2d6a5ce1fc52d19ca8ee7
Sarunas <sarunas.lekstutis@gmail.com>
2021-08-16 13:15:29 +0300

    reset password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,7 @@
+const crypto = require('crypto');
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const sgMail = require('@sendgrid/mail')
-
-// const sendGridTransport = require('nodemailer-sendgrid-transport');
-
-// const transporter = nodemailer.createTransport(sendGridTransport({
-//   auth: {
-//     api_key: 'process.env.SEND_GRID_API'
-//   }
-// }));
 
 exports.getLogin = (req, res, next) => {
   // const isLoggedIn = req.get('Cookie').split('=')[1];
@@ -72,47 +65,47 @@ exports.postLogin = (req, res, next) => {
 };
 
 exports.postSignup = (req, res, next) => {
-const email = req.body.email;
-const password = req.body.password;
-const confirmPassword = req.body.confirmPassword;
-User.findOne({
-    email: email
-  }).then(userDoc => {
-    if (userDoc) {
-      req.flash('error', 'Email exists already.');
-      return res.redirect('/signup');
-    }
-    return bcrypt.hash(password, 12).then(hashedPassword => {
-      const user = new User({
-        email: email,
-        password: hashedPassword,
-        cart: {
-          items: []
-        }
-      });
-      return user.save();
-  })
-  .then(result => {
-    res.redirect('/login');
-    sgMail.setApiKey(process.env.SEND_GRID_API)
-    const msg = {
-      to: email,
-      from: 'sarunas.lekstutis@gmail.com',
-      subject: 'Signup succeeded',
-      text: 'You successfully signed up.',
-      html: '<h1>You successfully signed up.</h1>'
-    }
-    sgMail.send(msg).then(() => {
-      console.log('Email sent');
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({
+      email: email
+    }).then(userDoc => {
+      if (userDoc) {
+        req.flash('error', 'Email exists already.');
+        return res.redirect('/signup');
+      }
+      return bcrypt.hash(password, 12).then(hashedPassword => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: {
+              items: []
+            }
+          });
+          return user.save();
+        })
+        .then(result => {
+          res.redirect('/login');
+          sgMail.setApiKey(process.env.SEND_GRID_API)
+          const msg = {
+            to: email,
+            from: 'sarunas.lekstutis@gmail.com',
+            subject: 'Signup succeeded',
+            text: 'You successfully signed up.',
+            html: '<h1>You successfully signed up.</h1>'
+          }
+          sgMail.send(msg).then(() => {
+            console.log('Email sent');
+          })
+        })
+        .catch(err => {
+          console.log(err);
+        });
     })
-  })
-  .catch(err => {
-    console.log(err);
-  });
-})
-.catch(err => {
-  console.log(err);
-});
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 exports.postLogout = (req, res, next) => {
@@ -120,4 +113,119 @@ exports.postLogout = (req, res, next) => {
     console.log(err);
     res.redirect('/');
   });
+};
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render('auth/reset', {
+    path: '/signup',
+    pageTitle: 'Reset',
+    errorMessage: message
+  });
+}
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect('/reset');
+    }
+    const token = buffer.toString('hex');
+    User.findOne({
+        email: req.body.email
+      })
+      .then(user => {
+        if (!user) {
+          req.flash('error', 'No account with that email found.');
+          return res.redirect('/reset');
+        }
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save();
+      })
+      .then(result => {
+        res.redirect('/');
+        sgMail.setApiKey(process.env.SEND_GRID_API)
+        const msg = {
+          to: req.body.email,
+          from: 'sarunas.lekstutis@gmail.com',
+          subject: 'Password Reset',
+          text: 'You successfully reset password.',
+          html: `
+        <p>You requested a password reset</p>
+        <p>Click this <a href="http://localhost:3000/reset/${token}">link</a>  to set a new password.</p>
+      `
+        }
+        sgMail.send(msg).then(() => {
+            console.log('Email sent');
+          })
+          .catch(err => {
+            console.log(err);
+          })
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  });
+};
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({
+      resetToken: token,
+      resetTokenExpiration: {
+        $gt: Date.now()
+      }
+    })
+    .then(user => {
+      let message = req.flash('error');
+      if (message.length > 0) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+      res.render('auth/new-password', {
+        path: '/new-password',
+        pageTitle: 'New password',
+        errorMessage: message,
+        userId: user._id.toString(),
+        passwordToken: token
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser;
+
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: {$gt: Date.now()},
+    _id: userId
+  })
+  .then(user => {
+    resetUser = user;
+    return bcrypt.hash(newPassword, 12);
+  })
+  .then(hashedPassword => {
+    resetUser.password = hashedPassword;
+    resetUser.resetToken = undefined;
+    resetUser.resetTokenExpiration = undefined;
+    return resetUser.save();
+  })
+  .then(result => {
+    res.redirect('/login');
+  })
+  .catch(err => {
+    console.log(err);
+  })
 };
