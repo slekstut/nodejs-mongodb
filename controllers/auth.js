cf44514cd44a56cccf5cfd4cbeb37f4c6e3762fe
Sarunas <sarunas.lekstutis@gmail.com>
2021-08-18 14:05:55 +0300

    validation finished

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,7 +19,11 @@ exports.getLogin = (req, res, next) => {
     path: '/login',
     pageTitle: 'Login',
     errorMessage: message,
-    validationErrors: []
+    validationErrors: [],
+    oldInput: {
+      email: '',
+      password: ' '
+    }
   });
 };
 
@@ -46,14 +50,18 @@ exports.getSignup = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  const errors = validationResult(req);
 
+  const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(422).render('auth/login', {
       path: '/login',
       pageTitle: 'Login',
       errorMessage: errors.array()[0].msg,
-      validationErrors: errors.array()
+      validationErrors: errors.array(),
+      oldInput: {
+        email: email,
+        password: password
+      }
     });
   }
 
@@ -62,8 +70,17 @@ exports.postLogin = (req, res, next) => {
     })
     .then(user => {
       if (!user) {
-        req.flash('error', 'Invalid email or password.');
-        return res.redirect('/login');
+        return res.status(422).render('auth/login', {
+          path: '/login',
+          pageTitle: 'Login',
+          errorMessage: 'Invalid email or password.',
+          validationErrors: [],
+          oldInput: {
+            email: email,
+            password: password
+          }
+        });
+
       }
       bcrypt.compare(password, user.password).then(doMatch => {
         if (doMatch) {
@@ -74,8 +91,17 @@ exports.postLogin = (req, res, next) => {
             res.redirect('/');
           })
         }
-        req.flash('error', 'Invalid email or password.');
-        res.redirect('/login');
+          return res.status(422).render('auth/login', {
+            path: '/login',
+            pageTitle: 'Login',
+            errorMessage: 'Invalid email or password.',
+            validationErrors: [],
+            oldInput: {
+              email: email,
+              password: password
+            }
+        });
+
       }).catch(err => {
         console.log(err);
         res.redirect('/login');
