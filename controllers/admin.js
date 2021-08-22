026a7930a1400c7a997a9fbc7d81d9ba8688936b
Sarunas <sarunas.lekstutis@gmail.com>
2021-08-22 14:54:55 +0300

    asyn req

--- controllers/admin.js
+++ controllers/admin.js
@@ -193,8 +193,8 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId).then(product => {
     if(!product) {
       return next(new Error('Product not found.'));
@@ -203,12 +203,10 @@ exports.postDeleteProduct = (req, res, next) => {
     return Product.deleteOne({ _id: prodId, userId: req.user._id })
   }).then(() => {
     console.log('DESTROYED PRODUCT');
-    res.redirect('/admin/products');
+    res.status(200).json({message: 'Success!'});
   })
   .catch(err => {
-    const error = new Error(err);
-    error.httpStatusCode = 500;
-    return next(error);
+    res.status(500).json({message: 'Deleting product failed.'});
   });
     
 };
